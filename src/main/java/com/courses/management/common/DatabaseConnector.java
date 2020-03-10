3cf95ce9da79f8b5bd97b579d61d4cccc0d42065
VladKharchenk0 <vlad.kharchenko55@gmail.com>
2020-03-10 17:06:41 +0200

    Moved database  connection to application.properties

--- src/main/java/com/courses/management/common/DatabaseConnector.java
+++ src/main/java/com/courses/management/common/DatabaseConnector.java
@@ -2,28 +2,41 @@ package com.courses.management.common;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseConnector {
     private static final HikariDataSource ds;
-
+private static final Logger LOGGER = LogManager.getLogger(DatabaseConnector.class);
     private DatabaseConnector() {
         throw new RuntimeException("This operation is not supported");
     }
 
     static {
         HikariConfig config = new HikariConfig();
-        config.setJdbcUrl("jdbc:postgresql://localhost:5432/course_management");
-        config.setUsername("postgres");
-        config.setPassword("1111");
+        final Properties properties = new Properties();
+        final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
+        try (InputStream inputStream = classLoader.getResourceAsStream("application.properties")) {
+            properties.load(inputStream);
+        } catch (Exception e) {
+            LOGGER.error("Error loading application.properties", e);
+            throw new RuntimeException("");
+        }
+
+        config.setJdbcUrl(properties.getProperty("jdbc.url"));
+        config.setUsername(properties.getProperty("jdbc.username"));
+        config.setPassword(properties.getProperty("jdbc.password"));
         ds = new HikariDataSource(config);
-        ds.setMaximumPoolSize(10);
+        ds.setMaximumPoolSize(Integer.parseInt(properties.getProperty("jdbc.connection.pool.max.size")));
     }
 
     public static HikariDataSource getConnector() {
-
         return ds;
     }
 }
