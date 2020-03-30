dcc6b269458112077ce5e90c2182c32c51913b2d
VladKharchenk0 <vlad.kharchenko55@gmail.com>
2020-03-30 19:25:18 +0300

    Added commands FindCourse, UpdateCourseStatus, UpdateCourseTitle, DeleteCourse.

--- src/main/java/com/courses/management/common/MainController.java
+++ src/main/java/com/courses/management/common/MainController.java
@@ -3,9 +3,7 @@ package com.courses.management.common;
 import com.courses.management.common.commands.Exit;
 import com.courses.management.common.commands.Help;
 import com.courses.management.common.commands.util.InputString;
-import com.courses.management.course.CreateCourse;
-import com.courses.management.course.FIndCourseByTitle;
-import com.courses.management.course.FindCourseById;
+import com.courses.management.course.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -26,8 +24,12 @@ public class MainController {
         commands = Arrays.asList(new CreateCourse(view),
                 new Help(view),
                 new Exit(view),
-                new FindCourseById(view),
-                new FIndCourseByTitle(view));
+                new FindCourse(view),
+                new UpdateCourseStatus(view),
+                new UpdateCourseTitle(view),
+                new DeleteCourse(view),
+                new ShowCourses(view)
+        );
     }
 
     public void read() {
@@ -42,24 +44,24 @@ public class MainController {
     private void doCommand(String input) {
         LOGGER.debug(String.format("doCommand: input=%s", input));
         InputString entry = new InputString(input);
-        for (Command command: commands) {
-           try {
-               if(command.canProcess(entry)){
-                   command.process(entry);
-                   break;
-               }
-           }catch (Exception e){
-               LOGGER.warn("doCommand. Warning "+ e.getMessage());
-               printError(e);
-               break;
+        for (Command command : commands) {
+            try {
+                if (command.canProcess(entry)) {
+                    command.process(entry);
+                    break;
+                }
+            } catch (Exception e) {
+                LOGGER.warn("doCommand. Warning " + e.getMessage());
+                printError(e);
+                break;
 
-           }
+            }
         }
     }
 
     private void printError(Exception e) {
         String message = e.getMessage();
-        view.write("Error because of "+ message);
+        view.write("Error because of " + message);
         view.write("Please try again");
     }
 
