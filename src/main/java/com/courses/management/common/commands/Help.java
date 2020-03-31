5ad3d9eb6613539533a07df0e62880b949b9618d
VladKharchenk0 <vlad.kharchenko55@gmail.com>
2020-03-31 18:22:55 +0300

    Updated Help class.

--- src/main/java/com/courses/management/common/commands/Help.java
+++ src/main/java/com/courses/management/common/commands/Help.java
@@ -4,6 +4,8 @@ import com.courses.management.common.Command;
 import com.courses.management.common.View;
 import com.courses.management.common.commands.util.InputString;
 import com.courses.management.course.CourseStatus;
+import com.courses.management.user.UserRole;
+import com.courses.management.user.UserStatus;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
@@ -51,6 +53,34 @@ public class Help implements Command {
         view.write("\t|\t-> move course to a DELETED status");
         view.write("---------------------------------------------------------");
 
+        view.write("\t| " + CREATE_USER);
+        view.write("\t|\t-> create new user, user role will be set as " + UserRole.NEWCOMER + " and user status");
+        view.write("\t|\t-> as " + UserStatus.NOT_ACTIVE);
+        view.write("---------------------------------------------------------");
+
+        view.write("\t| " + FIND_USER);
+        view.write("\t|\t-> find user with specified email");
+        view.write("---------------------------------------------------------");
+
+        view.write("\t| " + DELETE_USER_COURSE);
+        view.write("\t|\t-> delete user course and move user to NOT_ACTIVE status");
+        view.write("---------------------------------------------------------");
+
+        view.write("\t| " + FIND_ALL_USERS_BY_COURSE);
+        view.write("\t|\t-> get all users by specified course");
+        view.write("---------------------------------------------------------");
+
+        view.write("\t| " + UPDATE_USER_COURSE);
+        view.write("\t|\t-> update user course by specified course title");
+        view.write("---------------------------------------------------------");
+
+        view.write("\t| " + FIND_ALL_USERS_BY_STATUS);
+        view.write("\t|\t-> get all users by specified status ["+ collectUsersStatuses() +"]");
+        view.write("---------------------------------------------------------");
+
+        view.write("\t| " + UPDATE_USER_EMAIL);
+        view.write("\t|\t-> update user email");
+        view.write("---------------------------------------------------------");
 
         view.write("\t| " + EXIT);
         view.write("\t|\t-> exit application");
@@ -61,5 +91,9 @@ public class Help implements Command {
 
     private String collectCourseStatuses() {
         return Arrays.stream(CourseStatus.values()).map(CourseStatus::getStatus).collect(Collectors.joining(", "));
+    }
+
+    private String collectUsersStatuses() {
+        return Arrays.stream(UserStatus.values()).map(UserStatus::getStatus).collect(Collectors.joining(", "));
     }
 }
